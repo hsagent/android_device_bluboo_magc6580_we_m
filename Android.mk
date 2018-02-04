ad112b4053073642202036daa2cc700a2714d2b7
hsagent <xxxmapsxxx@mail.ru>
2018-02-04 14:47:58 +0300

    update

---
+++ Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+#ifeq ($(TARGET_DEVICE),magc6580_we_m)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+#endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_magc6580_we_m.mk
