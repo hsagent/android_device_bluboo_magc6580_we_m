14ea4ec6bbfd9f7dfa41b14c8ecb88118090dc06
hsagent <xxxmapsxxx@mail.ru>
2018-04-03 21:42:55 +0300

    init

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := magc6580_we_m
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/bluboo/magc6580_we_m/device_magc6580_we_m.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := magc6580_we_m
+PRODUCT_NAME := lineage_magc6580_we_m
+PRODUCT_BRAND := bluboo
+PRODUCT_MODEL := bluboo magc6580_we_m
+PRODUCT_MANUFACTURER := bluboo
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+# Use this to add magc6580_we_m to CM's lunch command menu
+for var in eng user userdebug; do
+  add_lunch_combo lineage_magc6580_we_m-$var
+done
